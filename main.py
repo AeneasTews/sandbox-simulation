ba8f5916832ba1f174a2536123d81710ed8f6f9b
Aeneas Tews <Aeneas.tews@gmail.com>
2024-01-27 15:51:14 +0100

    reformatting (PEP 8), refactoring of drawing pipeline (bottom to top and random left-right), debugging features
    - reformatting in order to comply with PEP 8
    - refactoring of drawing pipeline:
       Instead of creating and drawing on to a new grid, the existing grid is simply updated. This required the physics updates to run from the bottom row to the top. In addition a mechanism was implemented by which the checking order of the columns is randomized in order to get around "clumping" of cells due to unavailable space. The current implementation should 100% be considered a botch and not regarded as a permanent solution. However, the new implementation of the drawing system also enables water and liquid-physics. With regards to liquids, I may want to implement a method that checks whether there are any free cells in the row below and not the current random process. The current process works, but it's messy. Implemented some debugging features, such as debugging framerate and screenshots of every single frame.

--- main.py
+++ main.py
@@ -61,12 +61,18 @@ CELL_HEIGHT = size[1] / GRID_HEIGHT
 
 grid = [[(EMPTY, COLORS[EMPTY]) for x in range(GRID_WIDTH)] for y in range(GRID_HEIGHT)]
 
+# animation fix
+# this should absolutely be considered a botch
+change_col_checking_dir = True
+
 
 def draw_grid():
     for row in range(GRID_HEIGHT):
         for col in range(GRID_WIDTH):
             if grid[row][col][0] != EMPTY:
-                pg.draw.rect(screen, grid[row][col][1], ((col * CELL_WIDTH, row * CELL_HEIGHT), (CELL_WIDTH, CELL_HEIGHT)))
+                pg.draw.rect(screen, 
+                             grid[row][col][1],
+                             ((col * CELL_WIDTH, row * CELL_HEIGHT), (CELL_WIDTH, CELL_HEIGHT)))
 
 
 def add_cell():
@@ -87,22 +93,23 @@ def check_mouse():
 
 def update_grid():
     global grid
-    # create a new empty grid
-    next_grid = [[(EMPTY, COLORS[EMPTY]) for x in range(GRID_WIDTH)] for y in range(GRID_HEIGHT)]
-
-    # copy the bottom row to the new grid, since it isn't checked in the loops
-    next_grid[len(grid) - 1] = grid[len(grid) - 1]
 
     # check every cell for its value
     for row in range(GRID_HEIGHT - 1):
+        # reverse checking order this is necessary as falling objects would otherwise be checked multiple times per
+        # frame
+        row = GRID_HEIGHT - 2 - row
         for col in range(GRID_WIDTH):
+            # change counting order
+            if change_col_checking_dir:
+                col = GRID_WIDTH - 1 - col
 
             current_cell = grid[row][col]
 
             # check every cell, that isn't empty
             if current_cell[0] != EMPTY:
                 # copy all cells that have a value to the next grid
-                next_grid[row][col] = current_cell
+                # next_grid[row][col] = current_cell
 
                 # check falling cell types
                 if FALLING_TYPES.__contains__(current_cell[0]):
@@ -112,32 +119,31 @@ def update_grid():
 
                     # check if a cell doesn't have a filled cell below it
                     if grid[row + 1][col][0] == EMPTY:
-                        # empty the current cell and fill the one below it
-                        next_grid[row][col] = (EMPTY, COLORS[EMPTY])
-                        next_grid[row + 1][col] = current_cell
+                        grid[row][col] = (EMPTY, COLORS[EMPTY])
+                        grid[row + 1][col] = current_cell
 
                     # check if the outside columns are being checked
                     elif col != GRID_WIDTH - 1 and col != 0:
                         # check if the cell to the bottom right is empty
                         if grid[row + 1][col + drop_dir][0] == EMPTY:
-                            next_grid[row][col] = (EMPTY, COLORS[EMPTY])
-                            next_grid[row + 1][col + drop_dir] = current_cell
+                            grid[row][col] = (EMPTY, COLORS[EMPTY])
+                            grid[row + 1][col + drop_dir] = current_cell
 
                         # check if the cell to the bottom left is empty
                         elif grid[row + 1][col - drop_dir][0] == EMPTY:
-                            next_grid[row][col] = (EMPTY, COLORS[EMPTY])
-                            next_grid[row + 1][col - drop_dir] = current_cell
+                            grid[row][col] = (EMPTY, COLORS[EMPTY])
+                            grid[row + 1][col - drop_dir] = current_cell
 
                     # handle outside columns separately in order to avoid list index errors
                     else:
                         if col == 0:
                             if grid[row + 1][col + 1][0] == EMPTY:
-                                next_grid[row][col] = (EMPTY, COLORS[EMPTY])
-                                next_grid[row + 1][col + 1] = current_cell
+                                grid[row][col] = (EMPTY, COLORS[EMPTY])
+                                grid[row + 1][col + 1] = current_cell
                         else:
                             if grid[row + 1][col - 1][0] == EMPTY:
-                                next_grid[row][col] = (EMPTY, COLORS[EMPTY])
-                                next_grid[row + 1][col - 1] = current_cell
+                                grid[row][col] = (EMPTY, COLORS[EMPTY])
+                                grid[row + 1][col - 1] = current_cell
 
                 # if the type is stationary, nothing else should be checked
                 elif STATIONARY_TYPES.__contains__(grid[row][col][0]):
@@ -149,28 +155,41 @@ def update_grid():
 
                     if col != 0 and col != GRID_WIDTH - 1:
                         if grid[row + 1][col][0] == EMPTY:
-                            next_grid[row + 1][col] = current_cell
-                            next_grid[row][col] = (EMPTY, COLORS[EMPTY])
+                            grid[row + 1][col] = current_cell
+                            grid[row][col] = (EMPTY, COLORS[EMPTY])
+                            # next_grid[row + 1][col] = current_cell
+                            # next_grid[row][col] = (EMPTY, COLORS[EMPTY])
 
                         elif grid[row + 1][col - move_dir][0] == EMPTY:
-                            next_grid[row + 1][col - move_dir] = current_cell
-                            next_grid[row][col] = (EMPTY, COLORS[EMPTY])
+                            grid[row + 1][col - move_dir] = current_cell
+                            grid[row][col] = (EMPTY, COLORS[EMPTY])
+                            # next_grid[row + 1][col - move_dir] = current_cell
+                            # next_grid[row][col] = (EMPTY, COLORS[EMPTY])
 
                         elif grid[row + 1][col + move_dir][0] == EMPTY:
-                            next_grid[row + 1][col + move_dir] = current_cell
-                            next_grid[row][col] = (EMPTY, COLORS[EMPTY])
+                            grid[row + 1][col + move_dir] = current_cell
+                            grid[row][col] = (EMPTY, COLORS[EMPTY])
+                            # next_grid[row + 1][col + move_dir] = current_cell
+                            # next_grid[row][col] = (EMPTY, COLORS[EMPTY])
 
                         elif grid[row][col - move_dir][0] == EMPTY:
-                            next_grid[row][col - move_dir] = current_cell
-                            next_grid[row][col] = (EMPTY, COLORS[EMPTY])
+                            grid[row][col - move_dir] = current_cell
+                            grid[row][col] = (EMPTY, COLORS[EMPTY])
+                            # next_grid[row][col - move_dir] = current_cell
+                            # next_grid[row][col] = (EMPTY, COLORS[EMPTY])
 
                         elif grid[row][col + move_dir][0] == EMPTY:
-                            next_grid[row][col + move_dir] = current_cell
-                            next_grid[row][col] = (EMPTY, COLORS[EMPTY])
+                            grid[row][col + move_dir] = current_cell
+                            grid[row][col] = (EMPTY, COLORS[EMPTY])
+                            # next_grid[row][col + move_dir] = current_cell
+                            # next_grid[row][col] = (EMPTY, COLORS[EMPTY])
 
-    grid = next_grid
 
-
+# initialize debug stuff
+debug_frame_counter = 0
+debug = False
+debug_fps = 1
+game_fps = fps
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -192,6 +211,11 @@ while True:
 
             if event.key == pg.K_l:
                 paint = WATER
+
+            # debug mode
+            if event.key == pg.K_f:
+                debug = not debug
+                fps = debug_fps if fps == 60 else 60
 
             if event.key == pg.K_0:
                 cursor_size = 0
@@ -228,3 +252,11 @@ while True:
 
     pg.display.flip()
     clock.tick(fps)
+
+    # randomize / switch checking of columns
+    # change_col_checking_dir = not change_col_checking_dir
+    change_col_checking_dir = random.choice([True, False])
+
+    if debug:
+        pg.image.save(screen, f"./debug_images/{debug_frame_counter}.png")
+        debug_frame_counter += 1
